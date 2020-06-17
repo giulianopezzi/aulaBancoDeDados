6e774a2eaa98972b2f35ac557ab5eaa31ba27877
Giuliano F Pezzi <gfpezzi@gmail.com>
2020-06-16 23:33:26 -0300

    findById implementation

--- src/db/DB.java
+++ src/db/DB.java
@@ -10,17 +10,17 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-	
+
 	private static Connection conn = null;
 	
 	public static Connection getConnection() {
-		if(conn == null) {
+		if (conn == null) {
 			try {
 				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
 				conn = DriverManager.getConnection(url, props);
 			}
-			catch(SQLException e) {
+			catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
@@ -30,26 +30,26 @@ public class DB {
 	public static void closeConnection() {
 		if (conn != null) {
 			try {
-			conn.close();
-			}
-			catch(SQLException e) {
+				conn.close();
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
-
+	
 	private static Properties loadProperties() {
 		try (FileInputStream fs = new FileInputStream("db.properties")) {
-	        Properties props = new Properties();
-				props.load(fs);
-				return props;
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
 		}
-		catch(IOException e) {
+		catch (IOException e) {
+			System.out.println("Não encontrei o arquivo");
 			throw new DbException(e.getMessage());
 		}
 	}
 	
-	public static void closeStatements(Statement st) {
+	public static void closeStatement(Statement st) {
 		if (st != null) {
 			try {
 				st.close();
@@ -58,7 +58,7 @@ public class DB {
 			}
 		}
 	}
-	
+
 	public static void closeResultSet(ResultSet rs) {
 		if (rs != null) {
 			try {
